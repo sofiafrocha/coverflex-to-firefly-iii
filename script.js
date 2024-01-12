f19de06aee8908a04c4adf3ad8f9f7f6f0bf62a7
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2024-01-12 23:09:54 +0000

    Remove dotenv, use axios

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
-import * as dotenv from 'dotenv';
 
-const data = import('api_response.json');
-dotenv.config();
+const file = Bun.file('api_response.json');
+const data = await file.json();
 
-const fireflyToken = process.env.FIREFLY_API_KEY;
-const input = data.movements.list.slice(0, 100);
+const fireflyToken = Bun.env.FIREFLY_API_KEY;
+const input = data.movements.list.slice(0, 1);
 // console.log('input', input);
 
 const transactions = input.filter((t) => t.amount.amount > 0).map((t) => ({
@@ -19,29 +18,18 @@ const transactions = input.filter((t) => t.amount.amount > 0).map((t) => ({
     type: t.is_debit ? 'withdrawal' : 'deposit',
 }));
 
-const requests = transactions.map((t) =>
+transactions.forEach((t) => {
     axios.post('http://192.168.1.6/api/v1/transactions', {
         apply_rules: true,
         error_if_duplicate_hash: true,
         transactions: [t],
-    }, {
-        headers: { Authorization: `Bearer ${fireflyToken}` },
+    }, { headers: { 'Authorization': `Bearer ${fireflyToken}` }
+    }).then(function (response) {
+        console.log(response.data.data);
     })
-    .then(r => console.log('success: ', r.data.data.id))
-    .catch(e => console.log('error: ', e.response.data.message))
-);
-
-console.log('length', input.length)
-
-/*
-axios.post('http://192.168.1.6/api/v1/transactions', payload, {
-  headers: { Authorization: `Bearer ${fireflyToken}` },
-}).then((response) => {
-  console.log('hã', response)
-  console.log('📦 Firefly says:', response.data);
-	console.log('status', response.status)
-}).catch((err) => {
-  console.log('🚨 Error!', err);
-  console.log(err.response.data.errors)
+    .catch(function (error) {
+    console.log(error.response.data);
+    });
 });
-*/
+
+console.log('length', input.length);
